b250b3143fa04f07c96fcfa2c87264d96d593d21
Semion-hacker <obucheniekurs7@gmail.com>
2024-01-16 14:41:16 +0300

    feat: add header, add images

---
+++ Hangman/js/main.js
@@ -0,0 +1,10 @@
+let header = document.createElement('header');
+header.className ='header';
+header.innerHTML = 'Hangman';
+
+document.body.append(header);
+
+let main = document.createElement('main');
+main.className ='main';
+
+document.body.append(main);
