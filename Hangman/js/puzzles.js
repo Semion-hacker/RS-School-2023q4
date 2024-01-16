b799d1c916cee1353aa0d1defeb2243b9d5a3fe4
Semion-hacker <obucheniekurs7@gmail.com>
2024-01-16 18:05:24 +0300

    feat: add puzzles

---
+++ Hangman/js/puzzles.js
@@ -0,0 +1,34 @@
+const puzzles = [
+    {
+        mystery: "What is the most popular open source module builder for JavaScript applications?",
+        answer: "webpack"
+    },
+    {
+        mystery: "First widely used programming language.",
+        answer: "fortran"
+    },
+    {
+        mystery: "The most popular programming language in the world.",
+        answer: "javascript"
+    },
+    {
+        mystery: "What color is the snow?",
+        answer: "white"
+    },
+    {
+        mystery: "What color is the sky?",
+        answer: "blue"
+    },
+    {
+        mystery: "What color is the sun",
+        answer: "yellow"
+    },
+    {
+        mystery: "What color is the grass",
+        answer: "green"
+    },
+    {
+        mystery: "Name of the first programmer in the world.",
+        answer: "ada"
+    }
+];
